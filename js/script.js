3f9f0bc4cec5781a29cdaaa9bc35865d455d7026
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-03-21 17:49:44 +0100

    Space between tags in article has been added

--- js/script.js
+++ js/script.js
@@ -53,8 +53,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
-  
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';  
 
 function generateTitleLinks(customSelector = ''){
 
@@ -110,6 +110,10 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+
+  let allTags = [];
+
   /* find all articles */
 
   const articles = document.querySelectorAll(optArticleSelector);
@@ -158,15 +162,35 @@ function generateTags(){
 
       html = html + linkHTML;
 
+      /* [NEW] check if this link is NOT already in allTags */
+
+      if(allTags.indexOf(linkHTML) == -1){
+
+        /* [NEW] add generated code to allTags array */
+
+        allTags.push(linkHTML);
+      }
+
       /* END LOOP: for each tag */
 
-      /* insert HTML of all the links into the tags wrapper */
+    }
 
-      tagWrapper.innerHTML = html;
+    /* insert HTML of all the links into the tags wrapper */
+
+    tagWrapper.innerHTML = html;
 
     /* END LOOP: for every article: */
-    }
+
+    /* [NEW] find list of tags in right column */
+
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] add html from allTags to tagList */
+
+    tagList.innerHTML = allTags.join(' ');
+    
   }
+  
 }
 
 generateTags();
